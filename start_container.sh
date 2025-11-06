28cf4eac73b826434404bbef4ef02a8953aaf52a
Jramirez927 <36305701+Jramirez927@users.noreply.github.com>
2025-11-05 22:28:42 -0500

    Add echo statements for Docker pull and run

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,9 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+echo "Pulling From docker"
+docker pull jramirez927/python-zth-demo
 
 # Run the Docker image as a container
-echo
+echo "Running Image"
+docker run -d -p 5000:5000 jramirez927/python-zth-demo
